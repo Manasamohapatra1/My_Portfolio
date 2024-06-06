4591f0e1b1e7ce4fa86c3b318adf9f1d7ed817be
Manasamohapatra1 <126491891+Manasamohapatra1@users.noreply.github.com>
2024-06-06 22:08:21 +0530

    projects  modified

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,6 +5,7 @@ import FoodApp from "../Assets/Food.png";
 import Timer from "../Assets/trial.jpg";
 import Portfolio from "../Assets/portfolio.png";
 import calculator from "../Assets/calcula.jpg";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   const project = [
@@ -14,7 +15,7 @@ const Projects = () => {
       demoHref: "https://anime-saga.netlify.app",
       codeHref: "https://github.com/Manasamohapatra1/Anime-Web-App",
       child: (
-        <p className="text-center  text-gray-300 text-sm p-3">
+        <p className="text-center font-medium text-sm p-3">
           A fully responsive anime-website using html,css and javascript.And the
           datas are fetched from jikan api.{" "}
         </p>
@@ -26,7 +27,7 @@ const Projects = () => {
       demoHref: "https://my-moments.netlify.app",
       codeHref: "https://github.com/Manasamohapatra1/mygallery.github.io",
       child: (
-        <p className="text-center  text-gray-300 text-sm p-3">
+        <p className="text-center font-medium  text-sm p-3">
           A gallery-website containing images captured by me using html and css
           only.{" "}
         </p>
@@ -38,7 +39,7 @@ const Projects = () => {
       demoHref: "https://github.com/Manasamohapatra1/foodApp",
       codeHref: "https://github.com/Manasamohapatra1/foodApp",
       child: (
-        <p className="text-center text-gray-300 text-sm p-3">
+        <p className="text-center font-medium text-sm p-3">
           A android food app containing functional onboarding pages. Currently
           working on to make it fully functional.{" "}
         </p>
@@ -50,7 +51,7 @@ const Projects = () => {
       demoHref: " https://manasamohapatra1.github.io/timer.github.io/",
       codeHref: "https://github.com/Manasamohapatra1/timer.github.io",
       child: (
-        <p className="text-center text-gray-300 text-sm p-3">
+        <p className="text-center font-medium text-sm p-3">
           A responsive Stopwatch and timer website using js,css and html.{" "}
         </p>
       ),
@@ -61,7 +62,7 @@ const Projects = () => {
       demoHref: "http://my-portfolio-f01a6zkl8-manasamohapatra1.vercel.app",
       codeHref: "https://github.com/Manasamohapatra1/My_Portfolio",
       child: (
-        <p className="text-center text-gray-300 text-sm p-3">
+        <p className="text-center font-medium text-sm p-3">
           A fully functional responsive portfolio website using framework like
           React.js and Tailwind css.{" "}
         </p>
@@ -73,7 +74,7 @@ const Projects = () => {
       demoHref: "https://manasamohapatra1.github.io/calculator/",
       codeHref: "https://github.com/Manasamohapatra1/calculator",
       child: (
-        <p className="text-center text-gray-300 text-sm p-3">
+        <p className="text-center font-medium text-sm p-3">
           A simple calculator using javascript, html and css.{" "}
         </p>
       ),
@@ -89,17 +90,28 @@ const Projects = () => {
     window.open(codeHref, "_blank");
   };
   return (
-    <div name="project" className=" w-full text-white md:h-full">
+    <div name="project" className=" w-full md:h-full">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl text-center  text-emerald-200 font-bold  border-b-4 border-gray-500">
+        <motion.div
+          initial={{ opacity: 0, y: -180 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
+          className="pt-6 mt-[6rem]"
+        >
+          <p className="text-4xl md:text-5xl text-center font-bold  ">
             Projects
           </p>
-        </div>
+        </motion.div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className=" mt-[2.5rem] grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {project.map(({ id, src, child, demoHref, codeHref }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }}
+              key={id}
+              className="shadow-md shadow-gray-600 rounded-lg"
+            >
               <img
                 src={src}
                 alt=""
@@ -107,20 +119,20 @@ const Projects = () => {
               />
               <div className="flex items-center justify-center">
                 <button
-                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-cyan-300"
+                  className="w-1/2 font-semibold px-6 py-3 m-4 duration-200 hover:scale-105"
                   onClick={() => handleDemoClick(demoHref)}
                 >
                   Demo
                 </button>
                 <button
-                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-cyan-300"
+                  className="w-1/2 font-semibold px-6 py-3 m-4 duration-200 hover:scale-105"
                   onClick={() => handleCodeClick(codeHref)}
                 >
                   Code
                 </button>
               </div>
               {child}
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
